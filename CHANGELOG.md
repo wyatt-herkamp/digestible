96547c18bddd1d40a19b0cc47d03b2a04d8b657d
Wyatt Herkamp <wherkamp@gmail.com>
2023-10-13 09:54:24 -0400

    Implement Digestible for `std::collections::{HashMap, HashSet, BTreeMap}`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 ## 0.2.3 (Unreleased)
 - Generics are now supported in Macros
+- Implement Digestible for `std::collections::{HashMap, HashSet, BTreeMap}`
 ## 0.2.2 (2023-10-13) 
 - Fixed Unresolved path for `core::any`
 
--- digestible/src/digestible/std_types.rs
+++ digestible/src/digestible/std_types.rs
@@ -1,8 +1,36 @@
+use std::collections::{BTreeMap, HashMap, HashSet};
 use crate::digestible::internal_macros::impl_for_hashable_hack;
-use crate::Digestible;
+use crate::{Digestible, DigestWriter};
 use std::ffi::{OsStr, OsString};
 use std::path::{Path, PathBuf};
+use byteorder::ByteOrder;
 impl_for_hashable_hack!(OsStr);
 impl_for_hashable_hack!(OsString);
 impl_for_hashable_hack!(PathBuf);
 impl_for_hashable_hack!(Path);
+
+
+impl<K: Digestible, V: Digestible> Digestible for HashMap<K, V>{
+    fn digest<B: ByteOrder, W: DigestWriter>(&self, writer: &mut W) {
+        for (key, value) in self {
+            key.digest::<B, W>(writer);
+            value.digest::<B, W>(writer);
+        }
+    }
+}
+
+impl<K: Digestible, V: Digestible> Digestible for BTreeMap<K, V>{
+    fn digest<B: ByteOrder, W: DigestWriter>(&self, writer: &mut W) {
+        for (key, value) in self {
+            key.digest::<B, W>(writer);
+            value.digest::<B, W>(writer);
+        }
+    }
+}
+impl< V: Digestible> Digestible for HashSet<V>{
+    fn digest<B: ByteOrder, W: DigestWriter>(&self, writer: &mut W) {
+        for value in self {
+            value.digest::<B, W>(writer);
+        }
+    }
+}
